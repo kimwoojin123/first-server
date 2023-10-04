bfae59ee985a6489abb02221899990603755357d
kimwoojin <kimwoojin1903@gmail.com>
2023-10-04 17:00:48 +0900

    파이썬 경로지정해서 서버열기

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import http.server
+import socketserver
 
-httpd = http.server.HTTPServer(('0.0.0.0', 8080), http.server.SimpleHTTPRequestHandler)
+class CustomHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+PORT = 8080
 
-
-httpd.serve_forever()
+handler = CustomHttpRequestHandler
+server=socketserver.TCPServer(("", PORT), handler)
+server.serve_forever()
