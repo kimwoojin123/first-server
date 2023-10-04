396ccb63195af2fa8105a67c00a84843a7f0ebfd
kimwoojin <kimwoojin1903@gmail.com>
2023-10-04 16:25:44 +0900

    서버 열기 기본

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function(request, response){
+
+  console.log(request.method);
+  console.log(request.url);
+
+  let writeHeadObject = {
+    'Content-Type' : 'text/html'
+  }
+  response.writeHead(200, writeHeadObject);
+
+  fs.readFile("./public/index.html", function(err, data){
+    if(err){
+      console.error('파일을 읽지 못했습니다.');
+    } else {
+      response.end(data);
+    }
+  })
+
+}).listen(8080);
---
+++ app.py
@@ -0,0 +1,6 @@
+import http.server
+
+httpd = http.server.HTTPServer(('0.0.0.0', 8080), http.server.SimpleHTTPRequestHandler)
+
+
+httpd.serve_forever()
